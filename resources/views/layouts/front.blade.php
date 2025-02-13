78bde4dad3eb5e1b99940909826693c534b562b8
thernthy <thernthy2003@gmail.com>
2025-02-13 11:00:36 +0700

    add googleconsole profile

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,7 +2,7 @@
 <html lang="kh">
 <head>
     <meta charset="UTF-8">
-    <meta property="og:image" content="{{ asset('web_profile.jpg') }}">
+    <meta property="og:image" content="https://thernthy.site/web_profile.jpg">
     <meta property="og:title" content="THERNTHY | HOME">
     <meta property="og:description" content="Full Stack Developer and Dog Lover">
     <meta property="og:url" content="{{url('/')}}">
@@ -20,8 +20,8 @@
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Favicons -->
-    <link href="{{ asset('assets/thy/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-    <link rel="icon" href="{{ asset('web_profile.jpg') }}" type="image/x-icon">
+    {{-- <link href="{{ asset('assets/thy/img/apple-touch-icon.png') }}" rel="apple-touch-icon"> --}}
+    <link rel="icon" href="/web_profile.jpg" type="image/x-icon">
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets/thy/vendor/aos/aos.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/thy/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
@@ -211,8 +211,8 @@
         "@context": "https://schema.org",
         "@type": "Person",
         "name": "thernthy",
-        "url": "{{ url('/') }},
-        "image": "{{ asset('web_profile.jpg') }}"
+        "url": "https://thernthy.site",
+        "image": "https://thernthy.site/web_profile.jpg "
         }
     </script>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Duck Cloud Service</title>
+        <title>Thernthy Dashboard</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
