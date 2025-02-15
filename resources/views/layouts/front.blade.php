b7789b66abc9b7afbfe1756d42bae47e2189b4a7
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-15 10:11:34 +0700

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta property="og:image" content="https://thernthy.site/asset/thy/thernthy.site_42.jpg">
-    <link rel="icon" href="https://thernthy.site/asset/thy/thernthy.site_42.jpg" sizes="42x42" />
+    <link rel="icon" href="ttps://thernthy.site/assets/thy/thernthy.site_42.jpg" sizes="42x42" />
     <meta property="og:title" content="THERNTHY | HOME">
     <meta property="og:title" content="ធឿន​ ​ធី | Full Stack Developer">
     <meta property="og:description" content="Full Stack Developer and Dog Lover">
@@ -23,7 +23,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Favicons -->
     {{-- <link href="{{ asset('assets/thy/img/apple-touch-icon.png') }}" rel="apple-touch-icon"> --}}
-    <link rel="icon" href="https://thernthy.site/asset/thy/thernthy.site_42.jpg" type="image/x-icon">
+    <link rel="icon" href="ttps://thernthy.site/assets/thy/thernthy.site_42.jpg" type="image/x-icon">
     <!-- Vendor CSS Files -->
     <link href="{{ asset('assets/thy/vendor/aos/aos.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/thy/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
