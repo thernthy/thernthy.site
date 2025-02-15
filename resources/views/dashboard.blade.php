f5ccf955180574ae7bbd606b8d59441130420299
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-15 17:02:06 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="mx-auto">
-            <div class="overflow-hidden shadow-xl sm:rounded-lg" style="background:#ffffffc7;">
+            <div class="overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div>
         </div>
