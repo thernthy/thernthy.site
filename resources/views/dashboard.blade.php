348057501d4a521a96f6fb0c54dfc00badaebf87
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-15 17:32:50 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div>
-        <div class="mx-auto">
+        <div class="mx-auto" style="background:#02194fe0;">
             <div class="overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div>
