55fcc7e97f26f41e22fa65af06e9b0f366811c15
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-15 10:29:15 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>Duck Cloud Service</title>
+        <link rel="icon" href="https://thernthy.site/assets/thy/thernthy.site_42.jpg" type="image/x-icon" sizes="42x42">
+        <title>THY SEVER</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
