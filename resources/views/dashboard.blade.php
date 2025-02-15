65d2097270f5905771bcb9a558492c18a03f7450
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-15 16:40:42 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto">
             <div class="bg-[#ffffff9c] overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div>
