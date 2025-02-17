ba11db1066687409a5069b248f0678be49e4b9ba
thernthy <117651861+thernthy@users.noreply.github.com>
2025-02-17 10:31:24 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Actions\Fortify\LoginResponse as CustomLoginResponse;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+       $this->app->singleton(LoginResponse::class, CustomLoginResponse::class);
     }
 
     /**
